4167a9e176cbd5aa83c495fa0516d7b907c55af7
Diroun <33997746+Diroun@users.noreply.github.com>
2023-10-01 22:58:02 +0200

    New Files

---
+++ inc/Global.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+//Размер клетки
+constexpr unsigned char CELL_SIZE = 16;
+//Число столбцов
+constexpr unsigned char COLUMNS = 8;
+constexpr unsigned char EFFECT_DURATION = 16;
+//Размер шрифта по вертикали
+constexpr unsigned char FONT_HEIGHT = 16;
+//также по горизонтали
+constexpr unsigned char FONT_WIDTH = 8;
+//Число строк
+constexpr unsigned char ROWS = 8;
+//Размер экрана
+constexpr unsigned char SCREEN_RESIZE = 4;
+
+//Длительность для наших кадров
+constexpr unsigned short FRAME_DURATION = 16667;
+//количество мин
+constexpr unsigned short MINES = 8;
